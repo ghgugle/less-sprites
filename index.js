9e4ff23cc7b02c91be33c37174c3d209cd044360
Gurgen Ghukasyan <gurgen-ghukasyan@users.noreply.github.com>
2014-10-22 06:02:46 -0700

    Add support for retina icons.
    By adding "-x2" in the file name it will generate appropriated media queries into less file for that icons.

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ var Q = require('q');
 
 
 function Sprites() {
-	this.specs = {
-		appendRight: false
-	};
-	this.readArgs();
+    this.specs = {
+        appendRight: false
+    };
+    this.readArgs();
 }
 
 Sprites.prototype.createSprite = function(sourceDir, sourceFiles, destPath, lessPath,  baseUrl, prefix, noCache) {
@@ -34,81 +34,102 @@ Sprites.prototype.createSprite = function(sourceDir, sourceFiles, destPath, less
         }
     }
 
-	this.destPath = path.resolve(destPath);
-	this.lessPath = path.resolve(lessPath);
-	this.baseUrl = baseUrl;
-	this.prefix = prefix;
-	this.noCache = noCache;
+    this.destPath = path.resolve(destPath);
+    this.lessPath = path.resolve(lessPath);
+    this.baseUrl = baseUrl;
+    this.prefix = prefix;
+    this.noCache = noCache;
 
-	this.files = [];
-	this.spriteFile = im();
-	this.spriteFile.out('-background', 'none');
+    this.files = [];
+    this.spriteFile = im();
+    this.spriteFile.out('-background', 'none');
 
-	sourceFiles = this.getSourceFiles(sourceFiles);
-	if (!sourceFiles.length) {
-		throw new Error('No valid source files were provided.');
-	}
+    this.retinaSupportedFiles = [];
+    this.spriteFileRetina = im();
+    this.spriteFileRetina.out('-background', 'none');
+    this.retinaDestPath = ( this.destPath.substr(0, ( this.destPath.length - '.png'.length )) + '-x2.png' );
 
-	this.combine(sourceFiles)
-		.then(function() {
-			this.spriteFile.write(this.destPath, function(err) {
-				if (err) throw err;
+    sourceFiles = this.getSourceFiles(sourceFiles);
+
+    if (!sourceFiles.length) {
+        throw new Error('No valid source files were provided.');
+    }
+
+    this.combine(sourceFiles)
+        .then(function() {
+            if( this.retinaSupportedFiles.length > 0 ) {
+                this.spriteFileRetina.write(this.retinaDestPath, function(err) {
+                    if (err) throw err;
+                }.bind(this));
+            }
+            this.spriteFile.write(this.destPath, function(err) {
+                if (err) throw err;
                 this.writeStyles();
-			}.bind(this));
-		}.bind(this));
+            }.bind(this));
+        }.bind(this));
 };
 
 Sprites.prototype.getSourceFiles = function(files) {
-	var file,
-		sourceFiles = [];
+    var file,
+        sourceFiles = [];
 
-	for (var i = 0, l = files.length; i < l; i++) {
-		file = path.basename(files[i]);
-		if (file.match(/.*\.png$/i) && file != this.destPath) {
-			sourceFiles.push(file);
-		}
-	}
+    for (var i = 0, l = files.length; i < l; i++) {
+        file = path.basename(files[i]);
+        if (file.match(/.*\.png$/i) && file != this.destPath) {
+            sourceFiles.push(file);
+        }
+    }
 
-	return sourceFiles;
+    return sourceFiles;
 };
 
 Sprites.prototype.combine = function(files) {
-	var deferred = Q.defer();
-	async.each(files, this.processFile.bind(this), function(err) {
-		if (err) {
-			deferred.reject(new Error(err));
-		} else {
-			deferred.resolve();
-		}
-	});
-	return deferred.promise;
+    var deferred = Q.defer();
+    async.each(files, this.processFile.bind(this), function(err) {
+        if (err) {
+            deferred.reject(new Error(err));
+        } else {
+            deferred.resolve();
+        }
+    });
+    return deferred.promise;
 };
 
 Sprites.prototype.processFile = function(fileName, callback) {
-	var filePath = this.sourceDir + '/' + fileName;
-	if (!fs.existsSync(filePath)) {
-		throw new Error('Source file "' + filePath + '" does not exist.');
-	}
-	im(filePath).size(function(err, size) {
-		if (err) throw err;
-		this.spriteFile.append(filePath, this.specs.appendRight);
-		this.files.push({
-			name: fileName,
-			size: size
-		});
-		callback();
-	}.bind(this));
+    var filePath = this.sourceDir + '/' + fileName;
+    if (!fs.existsSync(filePath)) {
+        throw new Error('Source file "' + filePath + '" does not exist.');
+    }
+    im(filePath).size(function(err, size) {
+        if (err) throw err;
+
+        // Check for retina
+        if( fileName.toLowerCase().substr(-7, 7) == '-x2.png' ) {
+            this.spriteFileRetina.append(filePath, this.specs.appendRight);
+            this.retinaSupportedFiles.push( fileName.substr(0, (fileName.length - '-x2.png'.length)) + '.png' );
+        } else {
+            this.spriteFile.append(filePath, this.specs.appendRight);
+
+            this.files.push({
+                name: fileName,
+                size: size
+            });
+        }
+        
+        callback();
+    }.bind(this));
 };
 
 Sprites.prototype.writeStyles = function() {
     var that = this;
-	var relPath = path.relative(that.sourceDir, path.dirname(that.destPath));
-	var spriteFile = path.basename(that.destPath);
-	var date = new Date(),
-		imgVer = date.getFullYear().toString() + date.getMonth() + date.getDate() +  date.getHours() + date.getMinutes() + date.getSeconds();
-	var content = '';
-	var x = 0;
-	var y = 0;
+    var relPath = path.relative(that.sourceDir, path.dirname(that.destPath));
+    var spriteFile = path.basename(that.destPath);
+    var retinaSpriteFile = path.basename(that.retinaDestPath);
+    var date = new Date(),
+        imgVer = date.getFullYear().toString() + date.getMonth() + date.getDate() +  date.getHours() + date.getMinutes() + date.getSeconds();
+    var content = '';
+    var x = 0;
+    var y = 0;
     var spriteSize;
 
     im(that.destPath).size(function(err, size) {
@@ -119,57 +140,88 @@ Sprites.prototype.writeStyles = function() {
         };
 
         for (var i = 0, l = that.files.length; i < l; i++) {
-       		var file = that.files[i];
-       		content += util.format(
-       			'.%s(@sizePercent: 1;) {\n' +
-                       '\tdisplay: inline-block;\n' +
-                       '\twidth: (%dpx * @sizePercent);\n' +
-                       '\theight: (%dpx * @sizePercent);\n' +
-       				'\tbackground-image: url("%s%s");\n' +
-       				'\tbackground-position: (%dpx * @sizePercent) (%dpx * @sizePercent);\n' +
-       				'\tbackground-size: (%s * @sizePercent) (%s * @sizePercent);\n' +
-       			'}\n',
-                   that.prefix + file.name.toLowerCase().replace(/\.png/, ''),
-                   file.size.width,
-                   file.size.height,
-                   that.baseUrl,
-                   spriteFile + ( that.noCache ? '?'+imgVer: '' ),
-                   x, y,
-                   spriteSize.width, spriteSize.height
-       		);
-       		if (that.specs.appendRight) {
-       			x -= file.size.width;
-       		} else {
-       			y -= file.size.height;
-       		}
-       	}
+            var file = that.files[i],
+                hasRetinaSupport = ( that.retinaSupportedFiles.indexOf( file.name ) ) != -1;
 
-       	fs.writeFile(that.lessPath, content, function(err) {
-       		if (err) throw err;
-       	});
+            if( hasRetinaSupport ) {
+                content += util.format(
+                    '.%s(@sizePercent: 1;) {\n' +
+                    '\tdisplay: inline-block;\n' +
+                    '\twidth: (%dpx * @sizePercent);\n' +
+                    '\theight: (%dpx * @sizePercent);\n' +
+                    '\tbackground-image: url("%s%s");\n' +
+                    '\t@media screen and (-webkit-min-device-pixel-ratio:2), \n' +
+                                        '(min-device-pixel-ratio:2), \n' +
+                                        '(-webkit-min-device-pixel-ratio:1.5), \n' +
+                                        '(min-device-pixel-ratio:1.5) {\n' +
+                    '\t\tbackground-image: url("%s%s");\n' +
+                    '\t}\n' +
+                    '\tbackground-position: (%dpx * @sizePercent) (%dpx * @sizePercent);\n' +
+                    '\tbackground-size: (%s * @sizePercent) (%s * @sizePercent);\n' +
+                    '}\n',
+                    that.prefix + file.name.toLowerCase().replace(/\.png/, ''),
+                    file.size.width,
+                    file.size.height,
+                    that.baseUrl,
+                    spriteFile + ( that.noCache ? '?'+imgVer: '' ),
+                    that.baseUrl,
+                    retinaSpriteFile + ( that.noCache ? '?'+imgVer: '' ),
+                    x, y,
+                    spriteSize.width, spriteSize.height
+                );
+            } else {
+                content += util.format(
+                    '.%s(@sizePercent: 1;) {\n' +
+                    '\tdisplay: inline-block;\n' +
+                    '\twidth: (%dpx * @sizePercent);\n' +
+                    '\theight: (%dpx * @sizePercent);\n' +
+                    '\tbackground-image: url("%s%s");\n' +
+                    '\tbackground-position: (%dpx * @sizePercent) (%dpx * @sizePercent);\n' +
+                    '\tbackground-size: (%s * @sizePercent) (%s * @sizePercent);\n' +
+                    '}\n',
+                    that.prefix + file.name.toLowerCase().replace(/\.png/, ''),
+                    file.size.width,
+                    file.size.height,
+                    that.baseUrl,
+                    spriteFile + ( that.noCache ? '?'+imgVer: '' ),
+                    x, y,
+                    spriteSize.width, spriteSize.height
+                );
+            }
+
+            if (that.specs.appendRight) {
+                x -= file.size.width;
+            } else {
+                y -= file.size.height;
+            }
+        }
+
+        fs.writeFile(that.lessPath, content, function(err) {
+            if (err) throw err;
+        });
     });
 };
 
 Sprites.prototype.readArgs = function() {
-	var argv = process.argv.splice(2);
+    var argv = process.argv.splice(2);
 
-	if (!argv.length || argv[0] == '-h' || argv[0] == '--help') {
-		this.printUsage();
-		process.exit();
-	}
+    if (!argv.length || argv[0] == '-h' || argv[0] == '--help') {
+        this.printUsage();
+        process.exit();
+    }
 
-	var specsFile = argv[0];
+    var specsFile = argv[0];
 
-	if (!fs.existsSync(specsFile)) {
-		console.log('Error: Specs file "' + specsFile + '" does not exist.');
-		process.exit();
-	}
-	specsFile =  path.resolve(specsFile);
-	var specs = require(specsFile);
+    if (!fs.existsSync(specsFile)) {
+        console.log('Error: Specs file "' + specsFile + '" does not exist.');
+        process.exit();
+    }
+    specsFile =  path.resolve(specsFile);
+    var specs = require(specsFile);
 
     if (!specs['files'] && !specs['dir']) {
-   		throw new Error('Missing "files" or "dir" property.');
-   	}
+        throw new Error('Missing "files" or "dir" property.');
+    }
     if( !specs['dir'] ) {
         specs['dir'] = '.';
     }
@@ -189,53 +241,53 @@ Sprites.prototype.readArgs = function() {
     if( specs['sprite'] ) {
         if( specs['versioning'] ) {
             var date = new Date(),
-        		spriteVer =
+                spriteVer =
                     date.getFullYear().toString() + '.' +
-                        (date.getMonth() + 1) + '.' +
-                        date.getDate() + '.' +
-                        date.getHours() + '.' +
-                        (date.getMinutes() + 1) + '.' +
-                        (date.getSeconds() + 1);
+                    (date.getMonth() + 1) + '.' +
+                    date.getDate() + '.' +
+                    date.getHours() + '.' +
+                    (date.getMinutes() + 1) + '.' +
+                    (date.getSeconds() + 1);
 
             specs['sprite'] += '-' + spriteVer;
         }
         specs['sprite'] = specs['output_dir'] + specs['sprite'] + '.png';
     }
 
- 	// default directory is same as the json
-	if (!specs['sprite']) {
-		specs['sprite'] = path.basename(specsFile, '.json') + '.png';
-	}
-	// relative to the specsFile directory.
-	if (specs['sprite'][0] != '/') {
-		specs['sprite'] = path.dirname(specsFile) + '/' + specs['sprite'];
-	}
+    // default directory is same as the json
+    if (!specs['sprite']) {
+        specs['sprite'] = path.basename(specsFile, '.json') + '.png';
+    }
+    // relative to the specsFile directory.
+    if (specs['sprite'][0] != '/') {
+        specs['sprite'] = path.dirname(specsFile) + '/' + specs['sprite'];
+    }
 
-	if (!specs['less']) {
-		specs['less'] = path.basename(specsFile, '.json') + '.less';
-	}
+    if (!specs['less']) {
+        specs['less'] = path.basename(specsFile, '.json') + '.less';
+    }
 
-	if (specs['less'][0] != '/') {
-		specs['less'] = path.dirname(specsFile) + '/' + specs['less'];
-	}
+    if (specs['less'][0] != '/') {
+        specs['less'] = path.dirname(specsFile) + '/' + specs['less'];
+    }
 
-	if (specs['direction']) {
-		this.specs.appendRight = specs['direction'] == 'right';
-	}
+    if (specs['direction']) {
+        this.specs.appendRight = specs['direction'] == 'right';
+    }
 
-	this.createSprite(
-		path.resolve(specsFile, '..', specs['dir']),
-		specs['files'],
-		specs['sprite'],
-		specs['less'],
-		specs['base_url'],
-		specs['prefix'],
+    this.createSprite(
+        path.resolve(specsFile, '..', specs['dir']),
+        specs['files'],
+        specs['sprite'],
+        specs['less'],
+        specs['base_url'],
+        specs['prefix'],
         specs['nocache']
-	);
+    );
 };
 
 Sprites.prototype.printUsage = function() {
-	console.log('Usage: less-sprites sprite-specs.json');
+    console.log('Usage: less-sprites sprite-specs.json');
 };
 
 new Sprites();
